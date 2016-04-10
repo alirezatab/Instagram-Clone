b9e860914f7f9d667501a55d819ec0e72519c102
Eric <ericdhong>
2016-04-10 16:25:21 -0700

    Added a PostDetailVC to display the details of selected post in collection view

---
+++ Instagram/PostDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostDetailViewController.h
+//  Instagram
+//
+//  Created by Eric Hong on 4/10/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostDetailViewController : UIViewController
+
+@end
---
+++ Instagram/PostDetailViewController.m
@@ -0,0 +1,43 @@
+//
+//  PostDetailViewController.m
+//  Instagram
+//
+//  Created by Eric Hong on 4/10/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import "PostDetailViewController.h"
+
+@interface PostDetailViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+@implementation PostDetailViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"Hello, World";
+    
+    return cell;
+}
+
+- (IBAction)onBackButtonPressed:(UIBarButtonItem *)sender
+{
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+
+@end
