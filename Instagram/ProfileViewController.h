e76e52e7c039f54f67ac8f27374a5acec0fa25ff
Eric <ericdhong>
2016-04-09 22:03:57 -0700

    Added a .m file called ProfileVC into Profile Section Folder

---
+++ Instagram/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Instagram
+//
+//  Created by Eric Hong on 4/9/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
---
+++ Instagram/ProfileViewController.m
@@ -0,0 +1,52 @@
+//
+//  ProfileViewController.m
+//  Instagram
+//
+//  Created by Eric Hong on 4/9/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController () <UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableView.hidden = YES;
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tableViewCell" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *collectionCell = [collectionView dequeueReusableCellWithReuseIdentifier:@"collectionViewCell" forIndexPath:indexPath];
+    
+    return collectionCell
+}
+
+
+@end
