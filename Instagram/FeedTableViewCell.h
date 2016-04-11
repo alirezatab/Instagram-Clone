e5dcefb295f0211cb54ba434c776a25bfb2bd4b4
Chris Serra <csserra@gmail.com@gmail.com>
2016-04-10 22:06:38 -0700

    detail cell storyboard 2.0 + outlets

--- Instagram/FeedTableViewCell.h
+++ Instagram/FeedTableViewCell.h
@@ -9,16 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface FeedTableViewCell : UITableViewCell
-// top left
+// top
 @property (weak, nonatomic) IBOutlet UIImageView *topLeft_profileImageView;
 @property (weak, nonatomic) IBOutlet UIButton *topLeft_usernameButton;
 @property (weak, nonatomic) IBOutlet UIButton *topLeft_locationButton;
-// top right
 @property (weak, nonatomic) IBOutlet UILabel *topRight_timeLabel;
 // middle
 @property (weak, nonatomic) IBOutlet UIImageView *middle_mainImageView;
-// bottom right
+// bottom
 @property (weak, nonatomic) IBOutlet UIButton *bottomLeft_heartButton;
 @property (weak, nonatomic) IBOutlet UIButton *bottomLeft_commentButton;
+@property (weak, nonatomic) IBOutlet UIButton *bottomLeft_numLikesButton;
+@property (weak, nonatomic) IBOutlet UIButton *bottomLeft_commentUserButton;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLeft_commentTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLeft_commentDateLabel;
 
 @end
