c1409c2314ca492f8f0980e1cc5a36981e6788fd
Chris Serra <csserra@gmail.com@gmail.com>
2016-04-12 15:24:21 -0700

    merge dev-coredata into dev2

--- Instagram/MainTabBarViewController.m
+++ Instagram/MainTabBarViewController.m
@@ -13,9 +13,9 @@
 #import "Comment.h"
 #import "Picture.h"
 
-
-
-@interface MainTabBarViewController ()
+@interface MainTabBarViewController () <UITabBarControllerDelegate>
+@property NSManagedObjectContext *moc;
+@property NSArray *users;
 @end
 
 
@@ -64,7 +64,75 @@
 {
     UITabBarItem *barItem = [self.tabBar.items objectAtIndex:index];
     barItem.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+}
+
+#pragma mark - Navigation
+-(void) tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+    NSLog(@"[%@ %@]", self.class, NSStringFromSelector(_cmd));
+    ProfileViewController *dstVC = viewController;
+    dstVC.user = self.users[0];
+}
+
+
+
+#pragma mark - CoreData
+-(void)dummyData {
+    NSLog(@"[%@ %@]", self.class, NSStringFromSelector(_cmd));
+
+    // User
+    User *u = [NSEntityDescription insertNewObjectForEntityForName:@"User" inManagedObjectContext:self.moc];
+    u.username = @"heisenberg0";
+    u.fullname = @"Walter White";
+    u.bio = @"I've still got things left to do";
+
+    [self coreAddPicture:[UIImage imageNamed:@"heisenberg-image1"] withComment:@"laundry day" fromUser:u];
+    [self coreAddPicture:[UIImage imageNamed:@"heisenberg-image2"] withComment:@"kicking it with Jesse" fromUser:u];
+    [self coreSave];
     
+    self.users = @[u];
+    
+    // confirm that self.users[] got populated
+    User *u2 = self.users[0];
+    NSLog(@"user \"@%@\" has %i pictures", u2.username, u2.pictures.count);
+}
+-(void)coreAddPicture:(UIImage *)pictureImg withComment:(NSString *)commentStr fromUser:(User *)user {
+    Comment *c = [NSEntityDescription insertNewObjectForEntityForName:@"Comment" inManagedObjectContext:self.moc];
+    c.text = commentStr;
+    c.time = [NSDate date];
+    c.user = user;
+    
+    Picture *p = [NSEntityDescription insertNewObjectForEntityForName:@"Picture" inManagedObjectContext:self.moc];
+    p.image = UIImagePNGRepresentation(pictureImg);
+    p.location = @"somewhere in the desert, New Mexico";
+    p.time = [NSDate date];
+    p.owner = user;
+    [p addCommentsObject:c];
+    [p addLikedByObject:user];
+}
+-(void)coreLoadUsers {
+    NSLog(@"[%@ %@]", self.class, NSStringFromSelector(_cmd));
+    
+    NSFetchRequest *req = [[NSFetchRequest alloc] initWithEntityName:@"User"];
+    //req.sortDescriptors = @[[[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES]];
+    //    r.predicate = [NSPredicate predicateWithFormat:@"age >= 60 and budget >= 250"];
+    
+    NSError *error;
+    self.users = [[self.moc executeFetchRequest:req error:&error] mutableCopy];
+    if (!error) {
+        NSLog(@"core load ok: %lu items", self.users.count);
+    } else {
+        NSLog(@"core load error: %@", error);
+    }
+}
+-(void)coreSave {
+    NSLog(@"[%@ %@]", self.class, NSStringFromSelector(_cmd));
+    // save to Core
+    NSError *error;
+    if ([self.moc save:&error]) {
+        NSLog(@"core save ok");
+    } else {
+        NSLog(@"core save error: %@", error);
+    }
 }
 
 
