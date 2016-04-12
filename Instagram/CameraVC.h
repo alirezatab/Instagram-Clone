672e221223a6c2ea411c71e5a6efa85f415c5824
Alireza Tabrizi <alirezatabrizi@m.local>
2016-04-12 14:28:28 -0700

    camera is done, getting pics from library in done... next it UI and clean up the code, constraints

--- Instagram/CameraVC.h
+++ Instagram/CameraVC.h
@@ -10,10 +10,9 @@
 
 @interface CameraVC : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIButton *allPhotos;
 @property (weak, nonatomic) IBOutlet UIButton *takePhotoButton;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *cameraSegmentedControl;
+
 
 @property UIImage *choosenImage;
 
