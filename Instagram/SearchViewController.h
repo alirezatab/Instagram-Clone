f6f9580d22fa575edfe06047ac336fdc25baf791
Eric <ericdhong>
2016-04-12 18:49:12 -0700

    Added search bar and tableview to Search Controller

---
+++ Instagram/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  Instagram
+//
+//  Created by Eric Hong on 4/12/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ Instagram/SearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.m
+//  Instagram
+//
+//  Created by Eric Hong on 4/12/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"searchCell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"Hello";
+    
+    return cell;
+}
+
+
+
+@end
